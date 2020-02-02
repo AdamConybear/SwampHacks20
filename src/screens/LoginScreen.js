1b809304f54d7a19300ebc63bcd059499e700e3b
Adam Conybear <49500859+AdamConybear@users.noreply.github.com>
2020-02-02 06:35:04 -0500

    New login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,82 @@
 import React, {Component} from 'react';
-import { Text, StyleSheet,View,TouchableOpacity, AsyncStorage,Button,Image, Alert} from 'react-native';
+import { Text, StyleSheet,View,TextInput, TouchableOpacity,Button,Image, Alert} from 'react-native';
 
 export default class LoginScreen extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: ''
+        }
+    }
+
+    loadUser = (user) => {
+        this.props.navigation.navigate('Match',{userName:user})
+    }
+
     render(){
         return(
-            <Text>Login</Text>
+            <View>
+                <Text style={styles.about}>Enter your username</Text>
+                <TextInput  
+                    placeholder="fluffyPanda19"  
+                    underlineColorAndroid='transparent'  
+                    autoCapitalize = 'none'
+                    autoCorrect = {false}
+                    clearButtonMode = 'always'
+                    style={styles.inputStyle}  
+                    onChangeText = {username => this.setState({username})}
+                />
+
+               <View style={styles.buttonStyle}> 
+                    <TouchableOpacity style={styles.button} onPress = {() => this.loadUser(this.state.username)}>
+                        <Text style= {styles.Buttontext}>Enter</Text>
+                    </TouchableOpacity>
+                </View>
+
+
+            </View>
         );
     }
 
 };
+
+const styles = StyleSheet.create({
+    about: {
+        textAlign:'center',
+        fontSize: 24,
+        padding: 5,
+        fontWeight:'bold'
+        
+    },
+    inputStyle: {
+        padding: 10,
+        borderRadius: 7,
+        borderColor: '#26A69A',
+        borderWidth: 2,
+        marginBottom: 10
+
+    },
+    Buttontext: {
+        fontSize: 16,
+        padding: 5,
+        color: '#FFFFFF'
+
+    },
+    buttonStyle: {
+        //flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10
+
+    },
+    button:{
+        backgroundColor: '#000000',
+        borderColor: '#ffffff',
+        borderWidth: 1,
+        borderRadius: 12,
+        //https://codeburst.io/a-tale-of-three-buttons-exploring-react-native-styling-fa931159ef69
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+
+});
